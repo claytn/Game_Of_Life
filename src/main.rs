20e395b09b3747b1bc7e67928d210f8708052793
marsha88 <marsha88@purdue.edu>
2017-06-11 17:22:47 -0400

    Began modeling data structure to hold the grid. Wrote skeleton for updating the grid in a brute force manner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/* WARNING: BRUTE FORCE IMPLEMENTATION */
+
+/* Updates the current grid by applying rules of life */
+fn next(grid: [[bool; 8]; 8])->[[bool; 8]; 8]{
+	let updated_grid = [[bool; 8]; 8];
+	for i in 0..8{
+		for j in 0..8{
+			/*Rules 
+			  one or no neighbors dies			
+			  four or more neighbors dies
+			  two or three neighbors lives
+			
+			  cells with three neighors becomes populated
+			*/
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	/* Main Grid */
+  	let mut grid:[[bool;8];8] = [[false; 8]; 8];
 }
